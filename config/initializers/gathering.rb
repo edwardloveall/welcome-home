59199d1f863381bafa8135a3e7603fcfb4003892
Edward Loveall <edward@edwardloveall.com>
2012-11-25 20:18:13 -0500

    Removed gathering.rb config with api keys

--- config/initializers/gathering.rb
+++
@@ -1,4 +0,0 @@
-# Add your RDIO Consumer Key & Secret here for API Use
-# ref. http://developer.rdio.com/
-WelcomeHome::Application.config.rdio_consumer_key = ''
-WelcomeHome::Application.config.rdio_consumer_secret = ''
